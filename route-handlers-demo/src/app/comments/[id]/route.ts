9ba7913d1de34db99eb5f571a7e8f1708bf1de0c
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-16 00:22:03 +0530

    route handlers: delete request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -17,3 +17,13 @@ export async function PATCH(request: Request,
         comments[index].text = text;
         return Response.json(comments[index]);
     }
+
+export async function DELETE(_request: Request,
+    {params}: {params: Promise<{id: string}>} ){
+        const {id} = await params;
+        const index = comments.findIndex(comment=> comment.id ===parseInt(id));
+        const deletedComment = comments[index];
+        comments.splice(index,1);
+        return Response.json(deletedComment);
+    }
+
