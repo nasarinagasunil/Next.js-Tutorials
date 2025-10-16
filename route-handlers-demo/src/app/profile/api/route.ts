aedd41cfa8e10a6b1ec2a004b0fcfe5a741df142
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-16 13:33:22 +0530

    headers in request handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
-    return new Response("Profile Response");
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers)
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headersList = await headers();
+    console.log(headersList.get("Authorization"))
+    return new Response("<h1>Profile Response</h1>", {headers: {"Content-Type": "text/html"}});
 }
