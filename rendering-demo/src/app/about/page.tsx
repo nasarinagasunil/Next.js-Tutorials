6200bfbbb382fdb26973000eaa8b44a1659c965d
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-17 10:18:48 +0530

    server rendering strategies: static rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,4 @@
 export default function AboutPage() {
     console.log("rendering server component")
-    return <h1>About Page</h1>
+    return <h1>About Page {new Date().toLocaleTimeString()}</h1> // use npm run build and npm run start to verify static server rendering
 }
