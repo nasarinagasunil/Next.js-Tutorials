27dafe733415cbd79adc5b26273bd545afc15aa2
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-17 10:35:22 +0530

    strategies of server rendering: dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,13 @@
-export default function AboutPage() {
-    console.log("rendering server component")
+import { cookies } from "next/headers"
+
+export default async function AboutPage() {
+    
+    const cookiesStore = await cookies()
+    const theme = cookiesStore.get('theme')
+    console.log(theme)
     return <h1>About Page {new Date().toLocaleTimeString()}</h1> // use npm run build and npm run start to verify static server rendering
 }
+
+
+//Nextjs automaticcally enables it when it encounters a dynamic functions like cookies, headers, connection, 
+ // draftMode, after or searchParams props
