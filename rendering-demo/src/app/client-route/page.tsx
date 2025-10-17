b9a66e27969b37e465515484336d95376a22bcbd
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-17 16:39:49 +0530

    server-only code

---
+++ rendering-demo/src/app/client-route/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { serverSideFunction } from "@/utils/server-utils"
+export default function ClientRoute() {
+    const result = serverSideFunction();
+    return <h1>Client Route {result}</h1>
+}
