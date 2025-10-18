fbc61abd54dbc1ab2963bb163a2f6e37168eb1e0
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-19 00:54:53 +0530

    optimistic updates

--- data-fetching-demo/src/app/products-db/page.tsx
+++ data-fetching-demo/src/app/products-db/page.tsx
@@ -1,6 +1,6 @@
-import { removeProduct } from "@/actions/products";
+
 import { getProducts } from "@/prisma-db";
-import Link from "next/link";
+import { ProductDetails } from "./product-details";
 export type Product = {
     id: number;
     title: string;
@@ -10,22 +10,6 @@ export type Product = {
 
 export default async function ProductsDBPage() {
     const products: Product[] = await getProducts();
-    return (
-        <ul className="space-y-4 p-4">
-            {products.map((product) => (
-                <li key={product.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
-                    <Link href={`/products-db/${product.id}`}><h2 className="text-xl font-semibold">{product.title}</h2></Link>
-                    <p>{product.description}</p>
-                    <p className="text-lg font-medium">${product.price}</p>
-                    <form action={removeProduct.bind(null,product.id)}>
-                        <button type="submit" className="bg-red-400 px-2 py-1 text-white-500 rounded-md cursor-pointer hover:bg-red-500">
-                            Delete
-                        </button>
-                    </form>
 
-                </li>
-            ))
-            }
-        </ul >
-    )
+    return <ProductDetails products={products} />
 }
---
+++ data-fetching-demo/src/app/products-db/product-details.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { removeProduct } from "@/actions/products";
+import { getProducts } from "@/prisma-db";
+import Link from "next/link";
+import { useOptimistic } from "react";
+export type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string | null;
+};
+
+
+export const ProductDetails= ({products}: {products: Product []}) => {
+    
+    const [optimisticProducts, setOptimisticProducts] = useOptimistic(products, (currentProducts, productId) => {
+        return currentProducts.filter(product => product.id !== productId);
+    })
+
+    const removeProductById = (productId: number) => {
+        setOptimisticProducts(productId)
+        removeProduct(productId);
+    }
+
+    return (
+        <ul className="space-y-4 p-4">
+            {optimisticProducts.map((product) => (
+                <li key={product.id} className="p-4 bg-white shadow-md rounded-lg text-gray-700">
+                    <Link href={`/products-db/${product.id}`}><h2 className="text-xl font-semibold">{product.title}</h2></Link>
+                    <p>{product.description}</p>
+                    <p className="text-lg font-medium">${product.price}</p>
+                    <form action={removeProductById.bind(null, product.id)}>
+                        <button type="submit" className="bg-red-400 px-2 py-1 text-white-500 rounded-md cursor-pointer hover:bg-red-500">
+                            Delete
+                        </button>
+                    </form>
+
+                </li>
+            ))
+            }
+        </ul >
+    )
+}
