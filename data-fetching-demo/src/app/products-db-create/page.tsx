ac371bdb234dc17dbcf35d515c7f244d07a61666
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-18 17:27:13 +0530

    forms with server actions

---
+++ data-fetching-demo/src/app/products-db-create/page.tsx
@@ -0,0 +1,52 @@
+import { addProduct } from "@/prisma-db";
+import { redirect } from "next/navigation";
+
+export default function AddProductPage() {
+
+    async function createProduct (formData: FormData) {
+        "use server"
+        const title = formData.get("title") as string;
+        const price = formData.get("price") as string;
+        const description = formData.get("description") as string;
+
+        await addProduct(title, parseInt(price), description);
+        redirect("/products-db")
+
+    }
+
+    return (
+        <form action={createProduct}  className="p-4 space-y-4 max-w-96">
+            <label className="text-black">
+                Title
+                <input
+                    type="text"
+                    className="block w-full p-2 text-black border rounded"
+                    name="title"
+                    
+                />
+            </label>
+            <label className="text-black">
+                Price
+                <input
+                    type="number"
+                    className="block w-full p-2 text-black border rounded"
+                    name="price"
+                    
+                />
+            </label>
+            <label className="text-black">
+                Description
+                <textarea
+                    className="block w-full p-2 text-black border rounded"
+                    name="description"
+                    
+                />
+            </label>
+            <button
+                type="submit"
+                className="block w-full p-2 text-black bg-blue-500 rounded disabled:bg-gray-500">
+                Add Product
+            </button>
+        </form>
+    )
+}
